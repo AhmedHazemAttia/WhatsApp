d2cb9b8936151332a855743fbaefee256fbf9214
Ahmed <Ahmed_hazem.atia@hotmail.com>
2025-01-16 02:29:17 +0200

    Fixed(Lombok Issue by adding the version in the AnnotationProcessing) Created UserSynchrnonizer and Repo (APP WORKS)

---
+++ whatsappclone/src/main/java/com/ahmed/whatsappclone/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.ahmed.whatsappclone.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+import static com.ahmed.whatsappclone.user.UserConstants.FIND_USER_BY_EMAIL;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Query(name = FIND_USER_BY_EMAIL)
+    Optional<User> findByEmail(@Param("email") String userEmail);
+}
